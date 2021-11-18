cac6e45835b3522aa5efaa31b52826e819c9421f
erias04 <lichtbargeld@gmail.com>
2021-11-18 18:21:57 +0100

    move fake backend to App.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,9 @@
-import { useState } from 'react'
-
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at school',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30',
-            reminder: false,
-        }
-    ])
+const Tasks = ({ tasks }) => {
 
     return (
         <>
-          {tasks.map((tasks) => (
-          <h3 key={tasks.id}>{tasks.text}</h3>
+          {tasks.map((task) => (
+          <h3 key={task.id}>{task.text}</h3>
           ))}
         </>
     )
