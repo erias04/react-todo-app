228ea4374e39f659d884b953d7bdbf89b8f69259
erias04 <lichtbargeld@gmail.com>
2021-11-19 22:54:55 +0100

    get tasks from server we have an async function with a wait for fetching Tasks and then set the Tasks to the recieved tasks

--- src/App.js
+++ src/App.js
@@ -9,15 +9,21 @@ function App() {
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-
-      console.log(data);
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
 
-    fetchTasks()
+    getTasks()
   }, [/*user*/])
+
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   // Delete Task
   const deleteTask = (id) => {
