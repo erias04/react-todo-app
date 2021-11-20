1c06fb44e38d2763781d62ae5ea56fdd3f323555
erias04 <lichtbargeld@gmail.com>
2021-11-20 18:43:16 +0100

    create: Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; Elias Cerne 2021</p>
+            <a href="/about">About</a>
+        </footer>
+    )
+}
+
+export default Footfooter
